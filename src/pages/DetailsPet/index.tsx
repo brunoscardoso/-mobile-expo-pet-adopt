1742126a32c0b9634dc6dccb9b94b3d06663156f
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2020-09-07 19:32:34 -0300

    finish components on detailspet

--- src/pages/DetailsPet/index.tsx
+++ src/pages/DetailsPet/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import Icon from '@expo/vector-icons/Entypo';
+
 import {
   Container,
   Photo,
-  TextTitleCard,
-  TextDetailCard,
+  TextTitle,
+  TextSubTitle,
+  TextDetail,
+  TextDetailBold,
   Map,
   Circle,
+  AdoptButton,
+  ShareButton,
+  TextButton,
 } from './styles';
 
 import Header from '../../components/Header';
@@ -16,14 +23,29 @@ interface Props {
 }
 
 const DetailsPet: React.FC<Props> = ({ route }) => {
-  const { photo, name, gender, latitude, longitude } = route.params.item;
+  const {
+    id,
+    photo,
+    name,
+    gender,
+    size,
+    latitude,
+    longitude,
+  } = route.params.item;
+
+  // async function handleAdopt(): Promise<void> {}
+
   return (
     <>
       <Header />
       <Container>
-        <TextTitleCard>{name}</TextTitleCard>
+        <TextTitle>Uau!!! quer dizer...</TextTitle>
+        <TextSubTitle>{`Meu nome é ${name}, estou a procura de uma familía!`}</TextSubTitle>
         <Photo source={{ uri: photo }} />
-        <TextDetailCard>{gender}</TextDetailCard>
+        <TextDetailBold>{`Eu sou ${gender} de porte ${size}, fui visto pela última vez aqui nessa região...`}</TextDetailBold>
+        <TextDetail>
+          Vem me procurar? Estou com fome e ansioso pra te conhecer!
+        </TextDetail>
         <Map
           initialRegion={{
             latitude,
@@ -34,6 +56,14 @@ const DetailsPet: React.FC<Props> = ({ route }) => {
         >
           <Circle coordinate={{ latitude, longitude }} title={name} />
         </Map>
+        <AdoptButton>
+          <Icon name="heart" size={22} color="#fff" />
+          <TextButton>Eu adotei</TextButton>
+        </AdoptButton>
+        <ShareButton>
+          <Icon name="share" size={22} color="#fff" />
+          <TextButton>Compartilhar</TextButton>
+        </ShareButton>
       </Container>
     </>
   );
--- src/pages/DetailsPet/styles.ts
+++ src/pages/DetailsPet/styles.ts
@@ -1,24 +1,42 @@
 import styled from 'styled-components/native';
 import MapView, { Marker } from 'react-native-maps';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  margin: 24px 8px 10px 8px;
+  background: #fff;
+  border-radius: 8px;
+  padding: 16px 16px 16px 16px;
+`;
 
 export const Photo = styled.Image`
   width: 100%;
   height: 180px;
 `;
 
-export const TextTitleCard = styled.Text`
-  color: #000;
+export const TextTitle = styled.Text`
+  color: #322153;
   font-weight: bold;
-  padding-left: 4px;
-  font-size: 18px;
+  font-size: 22px;
 `;
 
-export const TextDetailCard = styled.Text`
-  color: #000;
-  font-size: 12px;
-  padding-left: 4px;
+export const TextSubTitle = styled.Text`
+  color: #322153;
+  font-weight: bold;
+  font-size: 14px;
+  margin-bottom: 8px;
+`;
+
+export const TextDetail = styled.Text`
+  color: #6c6c80;
+  font-size: 14px;
+  margin: 0 0 8px 0;
+`;
+
+export const TextDetailBold = styled.Text`
+  color: #6c6c80;
+  font-size: 14px;
+  font-weight: bold;
+  margin: 8px 0 8px 0;
 `;
 
 export const Map = styled(MapView)`
@@ -27,3 +45,32 @@ export const Map = styled(MapView)`
 `;
 
 export const Circle = styled(Marker)``;
+
+export const AdoptButton = styled.TouchableOpacity`
+  background: #f24e4e;
+  height: 50px;
+  flex-direction: row;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 16px;
+`;
+
+export const ShareButton = styled.TouchableOpacity`
+  background: #34cb79;
+  height: 50px;
+  flex-direction: row;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 8px 0px 16px 0;
+`;
+
+export const TextButton = styled.Text`
+  justify-content: center;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  padding-left: 10px;
+  font-size: 17px;
+`;
